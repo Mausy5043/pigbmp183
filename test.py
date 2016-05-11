ecef9821ebab17573939517795f3d7a30c90360d
Maurice (mausy5043) Hendrix <Mausy5043@users.noreply.github.com>
2016-05-11 15:48:45 +0200

    what works and what doesn't

--- test.py
+++ test.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 
 import pigpio
+import time
 
 pi = pigpio.pi()
 
 if pi.connected:
-   print("Connected to pigpio daemon.")
-   hndl = pi.spi_open(0, 32000)
-   (count, rx_data) = pi.spi_xfer(hndl, [0xAA, 0, 0])
-   # pi.spi_write(hndl, [0xAA])
-   # time.sleep(0.1)
-   # (count, rx_data) = pi.spi_read(hndl, 3)
-   pi.spi_close(hndl)
-   if count > 0:
-     AA = (rx_data[1] << 8) + rx_data[2]
-     print AA
+  print "Connected to pigpio daemon."
+  hndl = pi.spi_open(0, 1000000)
+  
+  ### THIS WORKS:
+  (cnt, rxd) = pi.spi_xfer(hndl, [0xAA, 0, 0])
+  if cnt > 0:
+    AA = (rxd[1] << 8) + rxd[2]
+    print AA
+   
+  ## THIS DOESN'T WORK:
+  pi.spi_write(hndl, [0xF4, 0x2E, 0])
+  time.sleep(0.045)
+
+  (cnt, rxd) = pi.spi_xfer(hndl, [0xF6,0,0])
+  if cnt > 0:
+    for i in rx_data:
+      print i
+
+  time.sleep(0.5)
+  pi.spi_close(hndl)
 
 pi.stop()
