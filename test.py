efc82ec6f999d4d4c7111a72ea80104edef9f38f
Mausy5043 <Mausy5043@users.noreply.github.com>
2016-05-11 15:51:43 +0200

    update

--- test.py
+++ test.py
@@ -8,20 +8,20 @@ pi = pigpio.pi()
 if pi.connected:
   print "Connected to pigpio daemon."
   hndl = pi.spi_open(0, 1000000)
-  
-  ### THIS WORKS:
+
+  # THIS WORKS:
   (cnt, rxd) = pi.spi_xfer(hndl, [0xAA, 0, 0])
   if cnt > 0:
     AA = (rxd[1] << 8) + rxd[2]
     print AA
-   
-  ## THIS DOESN'T WORK:
+
+  # THIS DOESN'T WORK:
   pi.spi_write(hndl, [0xF4, 0x2E, 0])
   time.sleep(0.045)
 
-  (cnt, rxd) = pi.spi_xfer(hndl, [0xF6,0,0])
+  (cnt, rxd) = pi.spi_xfer(hndl, [0xF6, 0, 0])
   if cnt > 0:
-    for i in rx_data:
+    for i in rxd:
       print i
 
   time.sleep(0.5)
